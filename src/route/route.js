fb80cb4155c64482e4755c9ec03b61356d11dda7
Priyanka <priyankakri8893@gmail.com>
2023-05-25 14:37:54 +0530

    router add

--- src/route/route.js
+++ src/route/route.js
@@ -5,4 +5,9 @@ const authorController=require('../Controller/authorController');
 
 router.post('/authors',authorController.createAuthor)
 router.get('/authordetails',authorController.getAllAuthors)
+router.post("/blogs", blogController.createBlog)
+router.get("/allblogs", blogController.getAllBlogs)
+router.get("/blogs", blogController.getFilteredBlogs)
+router.put("/blogs/:blogId", blogController.updateBlog)
+router.delete("/blogs/:blogId", blogController.deleteBlog)
 module.exports=router;
